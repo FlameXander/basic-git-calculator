a8354dbb02af4bd691ec89ac50d22120168cff5e
Alexander Fisunov <fisunov.a.v@gmail.com>
2025-01-10 20:20:06 +0300

    init

---
+++ src/main/java/ru/otus/java/git/calculator/CalculatorApplication.java
@@ -0,0 +1,31 @@
+package ru.otus.java.git.calculator;
+
+import java.util.Scanner;
+
+public class CalculatorApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+        System.out.println("Введите номер операции: 1 +, 2 -, 3 *, 4 /");
+        int operation = scanner.nextInt();
+        if (operation == 1) {
+            int result = a + b;
+            System.out.println(a + " + " + b + " = " + result);
+        }
+        if (operation == 2) {
+            int result = a - b;
+            System.out.println(a + " - " + b + " = " + result);
+        }
+        if (operation == 3) {
+            int result = a * b;
+            System.out.println(a + " * " + b + " = " + result);
+        }
+        if (operation == 4) {
+            int result = a / b;
+            System.out.println(a + " / " + b + " = " + result);
+        }
+    }
+}
